25744e6651658bc8d31e991c32b64ef8f436b56a
JeroTorne <tjerot@gmail.com>
2025-07-29 18:45:28 +0200

    debug false

--- corpuskinesio/settings.py
+++ corpuskinesio/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y(rooq8um)rjj+5_6rljhew7qxz5jo9u(dun6$qag!oigm^j&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['corpuskinesiologia.com.ar']
 
